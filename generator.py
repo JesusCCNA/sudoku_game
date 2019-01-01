3313f43bc6162d2ff7041c8c63c53cbd761fc6b0
JesusCCNA <40148024+JesusCCNA@users.noreply.github.com>
2019-01-01 11:27:35 -0400

    firs pygame code added

--- generator.py
+++ generator.py
@@ -1,10 +1,21 @@
 import random
-
+import pygame
 # Program that generates a  9x9 sudoku
+
+# Create GUI
+
+"""
+(width, height) = (600, 600)
+background_color = (252, 238, 220)
+screen = pygame.display.set_mode((width, height))
+screen.fill(background_color)
+pygame.display.set_caption("Sudoku Game - JesusCCNA ")
+pygame.display.flip()
+"""
+
 
 fields = {"A1" : random.randint(1,9),
         }
-
 
 for number in range(1,9):
     field_value = random.randint(1,9)
@@ -13,6 +24,3 @@ for number in range(1,9):
         field_value = random.randint(1,9)
 
     fields["A" + str(number + 1)] = field_value
-
-for n in range(1,10):
-    print(fields["A" + str(n)])
