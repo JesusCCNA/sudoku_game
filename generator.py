f7193d51f7754ccca11f0018a4d20e1bb725e060
JesusCCNA <40148024+JesusCCNA@users.noreply.github.com>
2019-01-01 11:07:11 -0400

    a with '#' will be ignored, and an empty message aborts the commit.

--- generator.py
+++ generator.py
@@ -1,1 +1,18 @@
-# Generate a  9x9 sudoku
+import random
+
+# Program that generates a  9x9 sudoku
+
+fields = {"A1" : random.randint(1,9),
+        }
+
+
+for number in range(1,9):
+    field_value = random.randint(1,9)
+
+    while field_value in fields.values():
+        field_value = random.randint(1,9)
+
+    fields["A" + str(number + 1)] = field_value
+
+for n in range(1,10):
+    print(fields["A" + str(n)])
